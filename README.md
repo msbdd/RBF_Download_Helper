5fef026f1b89005df66c68ba595549b6fb2fde27
Dmitry Sidorov-Biryukov <99192142+msbdd@users.noreply.github.com>
2025-10-09 19:15:57 +0200

    Added setup.py; Added readme badges for the build status;

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+![License: GPL v3](https://img.shields.io/badge/License-GPLv3-brightgreen.svg)
+![Code style: flake8](https://img.shields.io/badge/Code%20style-flake8-brightgreen)
+
+[![RBF_Download_Helper Build Windows](https://github.com/msbdd/RBF_Download_Helper/actions/workflows/Distribute_Windows.yml/badge.svg)](https://github.com/msbdd/RBF_Download_Helper/actions/workflows/Distribute_Windows.yml)
+
 A simple client for getting "Ringbuffer-alike" files from FDSNWS server.
---
+++ setup.py
@@ -0,0 +1,38 @@
+from cx_Freeze import setup, Executable
+import sys
+from pathlib import Path
+import obspy
+import os
+
+sys.setrecursionlimit(8000)
+
+exe = Executable(
+    script="main.py",
+    base="Console",
+)
+
+obsipy_data_dir = os.path.join(
+    os.path.dirname(obspy.__file__), "imaging", "data"
+    )
+
+site_packages = next(p for p in sys.path if 'site-packages' in p)
+dist_info = next(Path(site_packages).glob("obspy-*.dist-info"))
+setup(
+    name="RBF_Download_Helper",
+    version="0.1",
+    description="RBF_Download_Helper",
+    executables=[exe],
+    options={
+        "build_exe": {
+            "packages": ["obspy", "yaml", "numpy", "os", "pathlib"],
+            "include_files": [
+                (str(dist_info), f"lib/{dist_info.name}"),
+                (obsipy_data_dir, "lib/obspy/imaging/data"),
+                ("example_config.txt", "example_config"),
+                ("run_mulpitple.bat", "run_mulpitple.bat"),
+                ("LICENSE", "LICENSE"),
+            ],
+            "build_exe": "build/RBF_Download_Helper"
+        }
+    },
+)
